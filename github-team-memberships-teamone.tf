7c77c457e3dffb346446359ece563311fcec9c5e
Iris B <iris@ole.org>
2019-12-23 17:04:03 -0500

    Update github-team-memberships-teamone.tf

--- github-team-memberships-teamone.tf
+++ github-team-memberships-teamone.tf
@@ -3,8 +3,3 @@ resource "github_team_membership" "teamone-ArranHL" {
   team_id     = "${github_team.teamone.id}"
   role        = "member"
 }
-resource "github_team_membership" "teamone-1iristest" {
-  username    = "1iristest"
-  team_id     = "${github_team.teamone.id}"
-  role        = "member"
-}
